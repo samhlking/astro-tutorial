c2679fb59099a95a95844d5ba48f454d5e44f3e0
Sam King <sam33843@gmail.com>
2026-04-07 14:49:21 +1200

    Adding components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://samhlking.github.io/',
-    base: '/astro-tutorial'
+    base: '/astro-tutorial',
+    fonts: [{
+    provider: fontProviders.local(),
+    name: "Inter",
+    cssVariable: "--inter-font",
+    options: {
+      variants: [{
+        src: ['./src/assets/fonts/InterVariable.woff2'],
+        weight: 'normal',
+        style: 'normal'
+      }]
+    }
+  }]
 });
